93078757af19efef9865e9d3a9f46a3a8aafaaf5
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-02-21 19:52:31 -0500

    Renaming class for clarity

--- src/homework03/Main.java
+++
@@ -1,8 +0,0 @@
-package homework03;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/homework03/SortSearchers.java
@@ -0,0 +1,17 @@
+package homework03;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+// Main
+public class SortSearchers {
+
+    public static void main(String[] args) {
+
+        // Variables
+        ArrayList<Float> arrList = new ArrayList<>();
+
+
+    }
+}
+// End of main
