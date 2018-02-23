bf51b6c285574ec96e0a40671da6eaa3b54c0bd2
Kenneth Gajefski <kenneth.ag@outlook.com>
2018-02-23 08:18:06 -0500

    Final push. Output will now correctly display uppercase values in braille.

--- src/homework03/BrailleBuddies.java
+++ src/homework03/BrailleBuddies.java
@@ -41,6 +41,9 @@ public class BrailleBuddies {
                     {{ 0, 0}, { 0, 0}, { 0, 1}}   // upper case follows - 26
             };
 
+    // --------------------------------------------------------------------------------------------------
+    // fillArray
+    // --------------------------------------------------------------------------------------------------
     private static void fillArray(int arr[], String input){
         // Characters in the userInput string are being put into an Integer array, converting them
         // to ASCII values to be used in 3D array
@@ -50,6 +53,9 @@ public class BrailleBuddies {
         }
     }
 
+    // --------------------------------------------------------------------------------------------------
+    // main
+    // --------------------------------------------------------------------------------------------------
     public static void main(String[] args){
 
         // Variables
@@ -70,6 +76,7 @@ public class BrailleBuddies {
             for (int i = 0; i < userInput.length(); i++) {
                 // Casting letter at value of i in
                 int letter = userInput.charAt(i);
+                // ASCII values in params are for values of anything but space and letters both uppercase and lowercase
                 while ((letter < 65 && letter != 32) || (letter > 90 && letter < 97) || (letter > 172)) {
                     System.out.println("Invalid input. Input should not contain any special characters or numbers.");
                     userInput = keyboard.nextLine();
@@ -78,26 +85,29 @@ public class BrailleBuddies {
                 }
             }
 
+            // Declaring new integer array to score ASCII values of userInput
             size = userInput.length();
             int[] arr = new int[size];
 
+            // Filling the array with userInput
             fillArray(arr, userInput);
 
             // Outputting the header of each column
             System.out.printf(COL_LABEL, "Character:");
             for (int i = 0; i < arr.length; i++) {
                 if (arr[i] >= 0)
-                    System.out.printf(COL_HEAD, userInput.charAt(i));
+                    System.out.printf(COL_HEAD, userInput.charAt(i)); // Outputs character if not uppercase or space
                 else if (arr[i] == -65)
-                    System.out.printf(COL_HEAD, " ");
+                    System.out.printf(COL_HEAD, " "); // Outputting space characters
                 else {
-                    System.out.printf(COL_HEAD, "UP");
-                    char temp = userInput.charAt(i);
+                    System.out.printf(COL_HEAD, "UP"); // Since input has already been validated, this catches any uppercase
+                    char temp = userInput.charAt(i);   // letters, converts them to lowercase, and then outputs them
                     temp = Character.toLowerCase(temp);
                     System.out.printf(COL_HEAD, temp);
                 }
             }
 
+            // Outputting first row of numbers for braille conversion
             System.out.println();
             System.out.printf(COL_LABEL, "Row 1:");
             for (int i = 0; i < arr.length; i++) {
@@ -105,10 +115,16 @@ public class BrailleBuddies {
                     System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[arr[i]][0][0], BRAILLE_GRIDS[arr[i]][0][1]);
                 else if (arr[i] == -65)
                     System.out.printf(COL_HEAD, " ");
-                else
+                else {
+                    // If a value is not the space value, -65, or positive, then it must be uppercase since validation
+                    // has already occurred. UP is output first and followed by the uppercase value in arr[] + 32 since
+                    // that will make the value equivalent to what is in our 3D array of lowercase letters for braille
                     System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[26][0][0], BRAILLE_GRIDS[26][0][1]);
+                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[arr[i] + 32][0][0], BRAILLE_GRIDS[arr[i] + 32][0][1]);
+                }
             }
 
+            // Outputting second row of numbers for braille conversion
             System.out.println();
             System.out.printf(COL_LABEL, "Row 2:");
             for (int i = 0; i < arr.length; i++) {
@@ -116,10 +132,13 @@ public class BrailleBuddies {
                     System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[arr[i]][1][0], BRAILLE_GRIDS[arr[i]][1][1]);
                 else if (arr[i] == -65)
                     System.out.printf(COL_HEAD, " ");
-                else
-                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[26][0][0], BRAILLE_GRIDS[26][0][1]);
+                else {
+                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[26][1][0], BRAILLE_GRIDS[26][1][1]);
+                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[arr[i] + 32][1][0], BRAILLE_GRIDS[arr[i] + 32][1][1]);
+                }
             }
 
+            // Outputting third row of numbers for braille conversion
             System.out.println();
             System.out.printf(COL_LABEL, "Row 3:");
             for (int i = 0; i < arr.length; i++) {
@@ -127,15 +146,21 @@ public class BrailleBuddies {
                     System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[arr[i]][2][0], BRAILLE_GRIDS[arr[i]][2][1]);
                 else if (arr[i] == -65)
                     System.out.printf(COL_HEAD, " ");
-                else
-                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[26][0][0], BRAILLE_GRIDS[26][0][1]);
+                else {
+                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[26][2][0], BRAILLE_GRIDS[26][2][1]);
+                    System.out.printf(COL_ROW + COL_ROW, BRAILLE_GRIDS[arr[i] + 32][2][0], BRAILLE_GRIDS[arr[i] + 32][2][1]);
+                }
             }
 
+            // Prompting for user input again
+            System.out.println();
             System.out.println("Enter a word or sentence below to be converted to braille ('q' to exit): ");
             userInput = keyboard.nextLine();
 
         }
+        // End of sentinel loop
 
 
     }
+    // End of main
 }
